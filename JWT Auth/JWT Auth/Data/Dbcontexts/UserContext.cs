226976a1e13f9052ba35e158cc4ee7d1cbd9d5ae
Sabik <sabik.mextiyev98@gmail.com>
2023-11-12 17:17:51 +0400

    delete Name and add Username in Class User

--- JWT Auth/JWT Auth/Data/Dbcontexts/UserContext.cs
+++ JWT Auth/JWT Auth/Data/Dbcontexts/UserContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JWT_Auth.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
 
 namespace JWT_Auth.Data.Dbcontexts
 {
@@ -11,6 +13,37 @@ namespace JWT_Auth.Data.Dbcontexts
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
         }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=UsersDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var userBuilder = modelBuilder.Entity<User>();
+
+            userBuilder
+                .HasKey(x => x.Id);
+            userBuilder
+                .Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            userBuilder
+                .Property(x => x.Name)
+                .IsRequired();
+            userBuilder
+                .Property(x => x.Email)
+                .IsRequired();
+            userBuilder
+                .Property(x => x.Password)
+                .IsRequired();
+        }
+
+
 
     }
 }
