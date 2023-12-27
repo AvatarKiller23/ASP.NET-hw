44ead482b6a6bf43f782c2918513908a3e3d760c
Sabik <sabik.mextiyev98@gmail.com>
2023-12-27 20:26:26 +0400

    add class Game

---
+++ GameStore/GameStore/Data/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace GameStore.Data
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int GenreId { get; set; } // Жанр
+        public int CategoriesId { get; set; } // Категория
+
+    }
+}
